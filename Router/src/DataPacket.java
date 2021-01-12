65af09f7b85bfe042e3bb2a3dfe4bc045b08e856
Haliima Ahmed <43786387+HaliimaAhmed@users.noreply.github.com>
2021-01-11 22:18:34 -0500

    Add files via upload

---
+++ Router/src/DataPacket.java
@@ -0,0 +1,16 @@
+
+/*****************************************************************************************
+ * class DataPacket
+ 
+ ***************************************************************************************/
+public class DataPacket extends Packet {
+	
+	public boolean processFoundPacket(String a){
+		System.out.println("Sending packet out " + this.getPacketData() + " " + this.getDestNetwork() );
+		return true;
+	}
+	public boolean processNotFoundPacket(String b) {
+		System.out.println("Packet "+ this.getDestNetwork() +"is being dropped");
+		return false;
+	}
+}
